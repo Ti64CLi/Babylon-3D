800e03b284132b7080686d9babd278b6324fbbcc
Alexander <duzhar@hotmail.com>
2014-01-18 17:05:12 +0000

    mend

---
+++ Babylon/Tools/plane.h
@@ -0,0 +1,42 @@
+#ifndef BABYLON_PLANE_H
+#define BABYLON_PLANE_H
+
+#include <memory>
+#include <vector>
+
+#include "vector3.h"
+#include "matrix.h"
+
+using namespace std;
+
+namespace Babylon {
+
+	class Plane: public enable_shared_from_this<Plane> {
+
+	public:
+		typedef shared_ptr<Plane> Ptr;
+
+	public:
+		Vector3::Ptr normal;
+		float d;
+
+	public: 
+		Plane(float a, float b, float c, float d);		
+
+		// Methods
+		virtual void normalize();
+		virtual Plane::Ptr transform(Matrix::Ptr transformation);
+		virtual float dotCoordinate(Vector3::Ptr point);
+		virtual void copyFromPoints(Vector3::Ptr point1, Vector3::Ptr point2, Vector3::Ptr point3);
+		virtual bool isFrontFacingTo(Vector3::Ptr direction, float epsilon);
+		virtual float signedDistanceTo(Vector3::Ptr point);
+		// Statics
+		virtual Plane::Ptr FromArray(Float32Array array);
+		virtual Plane::Ptr FromPoints(Vector3::Ptr point1, Vector3::Ptr point2, Vector3::Ptr point3);
+		virtual Plane::Ptr FromPositionAndNormal(Vector3::Ptr origin, Vector3::Ptr normal);
+		virtual float SignedDistanceToPlaneFromPositionAndNormal(Vector3::Ptr origin, Vector3::Ptr normal, Vector3::Ptr point);
+	};
+
+};
+
+#endif // BABYLON_PLANE_H
