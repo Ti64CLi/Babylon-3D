b2de7b1b1678e25d6ad832cae801585afeed4c95
Alex D <duzhar@googlemail.com>
2014-08-19 16:57:05 +0100

    added native app for OpenGL

---
+++ BabylonNativeCs/BabylonGlut/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BabylonGlut
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    class Program
+    {
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static extern void glutInit(ref int argCount, byte[][] args);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+
+            var count = 0;
+            var argsBytes = new byte[0][];
+            glutInit(ref count, argsBytes);
+        }
+    }
+}
