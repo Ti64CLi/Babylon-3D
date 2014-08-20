6ae5b5987ec4592e9bea3ba7029eeaed5eb158b0
Alexander <duzhar@hotmail.com>
2014-08-20 22:04:02 +0100

    opengl is wokring

--- BabylonNativeCs/BabylonGlut/Program.cs
+++ BabylonNativeCs/BabylonGlut/Program.cs
@@ -21,10 +21,31 @@
         public static extern void glutMainLoop();
 
         [MethodImplAttribute(MethodImplOptions.Unmanaged)]
-        public static unsafe extern void glutKeyboardFunc(IntPtr func);
+        public static unsafe extern void glutDisplayFunc(void* display);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutPassiveMotionFunc(void* passiveMotion);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutMouseFunc(void* mouse);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutMotionFunc(void* motion);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutIdleFunc(void* idle);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutReshapeFunc(void* reshape);
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static unsafe extern void glutKeyboardFunc(void* key);
 
         [MethodImplAttribute(MethodImplOptions.Unmanaged)]
         public static extern void glutPostRedisplay();
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        public static extern void glutSwapBuffers();
 
         public const int GLUT_RGB = 0x0000;
         public const int GLUT_RGBA = 0x0000;
@@ -39,11 +60,45 @@
         public const int GLUT_STEREO = 0x0100;
         public const int GLUT_LUMINANCE = 0x0200;
 
+        public delegate void EmptyDelegate();
+        public delegate void TwoDimDelegate(int x, int y);
+        public delegate void MouseDelegate(int button, int state, int x, int y);
+        public delegate void KeyDelegate(byte key, int x, int y);
+
+        private static void display()
+        {
+            ////_main.render();
+            glutSwapBuffers();
+        }
+
+        private static void passiveMotion(int x, int y)
+        {
+            glutPostRedisplay();
+        }
+
         private static void key(byte k, int x, int y)
         {
-            Console.WriteLine("key pressed.");
-
             glutPostRedisplay();
+            throw new NotSupportedException();
+        }
+
+        private static void mouse(int button, int state, int x, int y)
+        {
+            glutPostRedisplay();
+        }
+
+        private static void idle()
+        {
+            glutPostRedisplay();
+        }
+
+        private static void resize(int w, int h)
+        {
+        }
+
+        private static void motion(int x, int y)
+        {
+            //_main.onMotion(x, y);
         }
 
         private static void Main(string[] args)
@@ -58,20 +113,16 @@
             glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
             glutCreateWindow(null /*"Babylon Native"*/);
 
-            /*
-            glutDisplayFunc(display);
-            glutPassiveMotionFunc(passiveMotion);
-            glutMouseFunc(mouse);
-            glutMotionFunc(motion);
-            glutIdleFunc(idle);
-            */
-
-            Action<byte, int, int> func = key;
-            glutKeyboardFunc((IntPtr)(Delegate)func);
-
-            /*
-            glutReshapeFunc(resize);
-            */
+            unsafe
+            {
+                glutDisplayFunc(((Delegate)new EmptyDelegate(display)).ToPointer());
+                glutPassiveMotionFunc(((Delegate)new TwoDimDelegate(passiveMotion)).ToPointer());
+                glutMouseFunc(((Delegate)new MouseDelegate(mouse)).ToPointer());
+                glutMotionFunc(((Delegate)new TwoDimDelegate(motion)).ToPointer());
+                glutIdleFunc(((Delegate)new EmptyDelegate(idle)).ToPointer());
+                glutKeyboardFunc(((Delegate)new KeyDelegate(key)).ToPointer());
+                glutReshapeFunc(((Delegate)new TwoDimDelegate(resize)).ToPointer());
+            }
 
             //_main.init();
             //_main.loadSceneTutorial4();
