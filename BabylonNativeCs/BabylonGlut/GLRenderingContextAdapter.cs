27a2ed0f2659d57b4471e4a10ef97b2b089216cb
Alexander <duzhar@hotmail.com>
2014-08-25 14:54:46 +0100

    glew ext added

--- BabylonNativeCs/BabylonGlut/GLRenderingContextAdapter.cs
+++ BabylonNativeCs/BabylonGlut/GLRenderingContextAdapter.cs
@@ -10,71 +10,87 @@
     {
         public const int GL_NO_ERROR = 0;
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern int glGetUniformLocation(uint program, char[] name);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern int __glewGetUniformLocation(uint program, char[] name);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public unsafe static extern void glBufferData(int target, int size, void* data, int usage);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public unsafe static extern void __glewBufferData(int target, int size, void* data, int usage);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glDepthMask(byte flag);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glLinkProgram(uint program);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewLinkProgram(uint program);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public unsafe static extern void glBufferSubData(int target, int offset, int size, void* data);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public unsafe static extern void __glewBufferSubData(int target, int offset, int size, void* data);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public unsafe static extern void glVertexAttribPointer(uint index, int size, int type, byte normalized, int stride, void* pointer);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public unsafe static extern void __glewVertexAttribPointer(uint index, int size, int type, byte normalized, int stride, void* pointer);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glEnableVertexAttribArray(uint index);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewEnableVertexAttribArray(uint index);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glCullFace(int mode);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glUniformMatrix4fv(int location, int count, byte transpose, float[] value);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewUniformMatrix4fv(int location, int count, byte transpose, float[] value);
 
-        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        public static extern uint glCreateProgram();
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern uint __glewCreateProgram();
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glDeleteShader(uint shader);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewDeleteShader(uint shader);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glEnable(int cap);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glGenBuffers(int n, uint[] buffers);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewGenBuffers(int n, uint[] buffers);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glUseProgram(uint program);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewUseProgram(uint program);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public unsafe static extern void glDrawElements(int mode, int count, int type, void* indices);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glGetShaderiv(uint shader, int pname, int[] @params);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewGetShaderiv(uint shader, int pname, int[] @params);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern byte[] glGetString(int name);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glGetShaderInfoLog(uint shader, int maxLength, int[] length, char[] infoLog);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewGetShaderInfoLog(uint shader, int maxLength, int[] length, char[] infoLog);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glGetIntegerv(int pname, int[] @params);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glShaderSource(uint shader, int count, char[] @string, int[] length);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewShaderSource(uint shader, int count, char[] @string, int[] length);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glBindBuffer(int target, int buffer);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewBindBuffer(int target, int buffer);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern int glGetAttribLocation(uint program, char[] name);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern int __glewGetAttribLocation(uint program, char[] name);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glReadPixels(int x, int y, int width, int height, int format, int type, byte[] data);
@@ -82,14 +98,17 @@
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glClear(int mode);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glUniform4f(int location, float x, float y, float z, float w);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewUniform4f(int location, float x, float y, float z, float w);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glUniform1i(int location, int x);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewUniform1i(int location, int x);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glGetProgramiv(uint program, int pname, int[] @params);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewGetProgramiv(uint program, int pname, int[] @params);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glDepthFunc(int func);
@@ -100,23 +119,28 @@
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glViewport(int x, int y, int width, int height);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glDeleteBuffers(int n, uint[] buffers);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewDeleteBuffers(int n, uint[] buffers);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glAttachShader(uint program, uint shader);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewAttachShader(uint program, uint shader);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glCompileShader(uint shader);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewCompileShader(uint shader);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern void glClearColor(float red, float green, float blue, float alpha);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern void glUniform3f(int location, float x, float y, float z);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern void __glewUniform3f(int location, float x, float y, float z);
 
-        [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
-        public static extern uint glCreateShader(int shaderType);
+        [DllImport("glew", CallingConvention = CallingConvention.StdCall)]
+        [MethodImpl(MethodImplOptions.Unmanaged | MethodImplOptions.ForwardRef)]
+        public static extern uint __glewCreateShader(int shaderType);
 
         [DllImport("opengl", CallingConvention = CallingConvention.StdCall)]
         public static extern int glGetError();
@@ -166,7 +190,7 @@
 
         public Web.WebGLUniformLocation getUniformLocation(Web.WebGLProgram program, string name)
         {
-            var glUniformLocation = new GlUniformLocation(glGetUniformLocation(program.Value, name.ToCharArray()));
+            var glUniformLocation = new GlUniformLocation(__glewGetUniformLocation(program.Value, name.ToCharArray()));
             ErrorTest();
             return glUniformLocation;
         }
@@ -182,7 +206,7 @@
             {
                 fixed (void* pdata = data)
                 {
-                    glBufferData(target, data.Length * sizeof(float), pdata, usage);
+                    __glewBufferData(target, data.Length * sizeof(float), pdata, usage);
                 }
             }
 
@@ -195,7 +219,7 @@
             {
                 fixed (void* pdata = data)
                 {
-                    glBufferData(target, data.Length * sizeof(ushort), pdata, usage);
+                    __glewBufferData(target, data.Length * sizeof(ushort), pdata, usage);
                 }
             }
 
@@ -230,7 +254,7 @@
 
         public void linkProgram(Web.WebGLProgram program)
         {
-            glLinkProgram(program.Value);
+            __glewLinkProgram(program.Value);
             ErrorTest();
         }
 
@@ -243,7 +267,7 @@
         {
             unsafe
             {
-                glBufferSubData(target, offset, size, data.ToPointer());
+                __glewBufferSubData(target, offset, size, data.ToPointer());
             }
         }
 
@@ -256,7 +280,7 @@
         {
             unsafe
             {
-                glVertexAttribPointer((uint)indx, size, type, (byte)(normalized ? 1 : 0), stride, new IntPtr(offset).ToPointer());
+                __glewVertexAttribPointer((uint)indx, size, type, (byte)(normalized ? 1 : 0), stride, new IntPtr(offset).ToPointer());
             }
 
             ErrorTest();
@@ -289,7 +313,7 @@
 
         public void enableVertexAttribArray(int index)
         {
-            glEnableVertexAttribArray((uint)index);
+            __glewEnableVertexAttribArray((uint)index);
             ErrorTest();
         }
 
@@ -311,7 +335,7 @@
 
         public void uniformMatrix4fv(Web.WebGLUniformLocation location, bool transpose, float[] value)
         {
-            glUniformMatrix4fv((int)location.Value, value.Length / 16, (byte)(transpose ? 1 : 0), value);
+            __glewUniformMatrix4fv((int)location.Value, value.Length / 16, (byte)(transpose ? 1 : 0), value);
             ErrorTest();
         }
 
@@ -357,14 +381,14 @@
 
         public Web.WebGLProgram createProgram()
         {
-            var glProgramAdapter = new GlProgramAdapter(glCreateProgram());
+            var glProgramAdapter = new GlProgramAdapter(__glewCreateProgram());
             ErrorTest();
             return glProgramAdapter;
         }
 
         public void deleteShader(Web.WebGLShader shader)
         {
-            glDeleteShader(shader.Value);
+            __glewDeleteShader(shader.Value);
             ErrorTest();
         }
 
@@ -412,7 +436,7 @@
         public Web.WebGLBuffer createBuffer()
         {
             uint[] buffers = new uint[1];
-            glGenBuffers(1, buffers);
+            __glewGenBuffers(1, buffers);
             ErrorTest();
             return new GlBufferAdapter(buffers[0]);
         }
@@ -424,7 +448,7 @@
 
         public void useProgram(Web.WebGLProgram program)
         {
-            glUseProgram(program.Value);
+            __glewUseProgram(program.Value);
             ErrorTest();
         }
 
@@ -533,7 +557,7 @@
             var GL_INFO_LOG_LENGTH = 35716;
             //var GL_SHADING_LANGUAGE_VERSION = 35724;
             var k = new int[1];
-            glGetShaderiv(shader.Value, GL_INFO_LOG_LENGTH, k);
+            __glewGetShaderiv(shader.Value, GL_INFO_LOG_LENGTH, k);
             if (k[0] == -1)
             {
                 return string.Empty;
@@ -545,7 +569,7 @@
             }
 
             var result = new char[k[0]];
-            glGetShaderInfoLog(shader.Value, k[0], k, result);
+            __glewGetShaderInfoLog(shader.Value, k[0], k, result);
 
             ////var version = glGetString(GL_SHADING_LANGUAGE_VERSION);
 
@@ -635,7 +659,7 @@
             var chars = source.ToCharArray();
             var len = new int[1];
             len[0] = chars.Length;
-            glShaderSource(shader.Value, 1, chars, len);
+            __glewShaderSource(shader.Value, 1, chars, len);
             ErrorTest();
         }
 
@@ -651,13 +675,13 @@
 
         public void bindBuffer(int target, Web.WebGLBuffer buffer)
         {
-            glBindBuffer(target, (int)(buffer != null ? buffer.Value : 0));
+            __glewBindBuffer(target, (int)(buffer != null ? buffer.Value : 0));
             ErrorTest();
         }
 
         public int getAttribLocation(Web.WebGLProgram program, string name)
         {
-            var attribLocation = glGetAttribLocation(program.Value, name.ToCharArray());
+            var attribLocation = __glewGetAttribLocation(program.Value, name.ToCharArray());
             ErrorTest();
             return attribLocation;
         }
@@ -776,7 +800,7 @@
 
         public void uniform4f(Web.WebGLUniformLocation location, double x, double y, double z, double w)
         {
-            glUniform4f((int)location.Value, (float)x, (float)y, (float)z, (float)w);
+            __glewUniform4f((int)location.Value, (float)x, (float)y, (float)z, (float)w);
             ErrorTest();
         }
 
@@ -792,14 +816,14 @@
 
         public void uniform1i(Web.WebGLUniformLocation location, int x)
         {
-            glUniform1i(location.Value, x);
+            __glewUniform1i(location.Value, x);
             ErrorTest();
         }
 
         public object getProgramParameter(Web.WebGLProgram program, int pname)
         {
             var i = new int[1];
-            glGetProgramiv(program.Value, pname, i);
+            __glewGetProgramiv(program.Value, pname, i);
             ErrorTest();
             return i[0] == 0 ? (object)null : i[0];
         }
@@ -898,7 +922,7 @@
         public object getShaderParameter(Web.WebGLShader shader, int pname)
         {
             var i = new int[1];
-            glGetShaderiv(shader.Value, pname, i);
+            __glewGetShaderiv(shader.Value, pname, i);
             ErrorTest();
             return i[0] == 0 ? (object)null : i[0];
         }
@@ -942,7 +966,7 @@
 
         public void deleteBuffer(Web.WebGLBuffer buffer)
         {
-            glDeleteBuffers(1, new uint[] { buffer.Value });
+            __glewDeleteBuffers(1, new uint[] { buffer.Value });
             ErrorTest();
         }
 
@@ -968,13 +992,13 @@
 
         public void attachShader(Web.WebGLProgram program, Web.WebGLShader shader)
         {
-            glAttachShader(program.Value, shader.Value);
+            __glewAttachShader(program.Value, shader.Value);
             ErrorTest();
         }
 
         public void compileShader(Web.WebGLShader shader)
         {
-            glCompileShader(shader.Value);
+            __glewCompileShader(shader.Value);
             ErrorTest();
         }
 
@@ -1016,7 +1040,7 @@
 
         public void uniform3f(Web.WebGLUniformLocation location, double x, double y, double z)
         {
-            glUniform3f((int)location.Value, (float)x, (float)y, (float)z);
+            __glewUniform3f((int)location.Value, (float)x, (float)y, (float)z);
             ErrorTest();
         }
 
@@ -1047,7 +1071,7 @@
 
         public Web.WebGLShader createShader(int type)
         {
-            var shader = (uint)glCreateShader(type);
+            var shader = (uint)__glewCreateShader(type);
             ErrorTest();
             return new GlShaderAdapter(shader);
         }
