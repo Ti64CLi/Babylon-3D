0171db90244b53485ba34884620413412af990a1
Alexander <duzhar@hotmail.com>
2014-09-09 22:13:53 +0100

    some adjustments

--- BabylonNativeCs/BabylonNativeCsLibrary/Cameras/babylon.camera.cs
+++ BabylonNativeCs/BabylonNativeCsLibrary/Cameras/babylon.camera.cs
@@ -192,46 +192,18 @@ namespace BABYLON
 
             var engine = this.getEngine();
             // TODO: this code causes issue with 'select'
-            ////if (this.mode == PERSPECTIVE_CAMERA)
-            ////{
-            ////    check = this._cache.fov == this.fov && this._cache.aspectRatio == engine.getAspectRatio(this);
-            ////}
-            ////else
-            ////{
-            ////    check = this._cache.orthoLeft == this.orthoLeft && this._cache.orthoRight == this.orthoRight && this._cache.orthoBottom == this.orthoBottom
-            ////            && this._cache.orthoTop == this.orthoTop && this._cache.renderWidth == engine.getRenderWidth()
-            ////            && this._cache.renderHeight == engine.getRenderHeight();
-            ////}
-
-            ////return check;
-
             if (this.mode == PERSPECTIVE_CAMERA)
             {
-                return this._cache.fov == this.fov && this._cache.aspectRatio == engine.getAspectRatio(this);
+                check = this._cache.fov == this.fov && this._cache.aspectRatio == engine.getAspectRatio(this);
+            }
+            else
+            {
+                check = this._cache.orthoLeft == this.orthoLeft && this._cache.orthoRight == this.orthoRight && this._cache.orthoBottom == this.orthoBottom
+                        && this._cache.orthoTop == this.orthoTop && this._cache.renderWidth == engine.getRenderWidth()
+                        && this._cache.renderHeight == engine.getRenderHeight();
             }
 
-            return cacheTest(engine);
-        }
-
-        private bool cacheTest(Engine engine)
-        {
-            var res2 = ex1(engine) && ex2(engine) && ex3(engine);
-            return res2;
-        }
-
-        private bool ex1(Engine engine)
-        {
-            return this._cache.orthoLeft == this.orthoLeft && this._cache.orthoRight == this.orthoRight;
-        }
-
-        private bool ex2(Engine engine)
-        {
-            return this._cache.orthoBottom == this.orthoBottom && this._cache.orthoTop == this.orthoTop;
-        }
-
-        private bool ex3(Engine engine)
-        {
-            return this._cache.renderWidth == engine.getRenderWidth() && this._cache.renderHeight == engine.getRenderHeight();
+            return check;
         }
 
         /// <summary>
